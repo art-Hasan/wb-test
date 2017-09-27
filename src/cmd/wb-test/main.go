4e8857d4f042f200fe8b06e0b1f29a0d69c091c5
art-Hasan <guseyin.gasimov@gmail.com>
2017-09-27 18:42:27 +0300

    Updated main.go

--- src/cmd/wb-test/main.go
+++ src/cmd/wb-test/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 	"sync"
 )
 
@@ -16,61 +15,74 @@ func init() {
 	log.SetFlags(0)
 }
 
+func HandleUri(uri string, total *int, wg *sync.WaitGroup, mux *sync.Mutex) {
+	// Decrement the counter when the goroutine completes.
+	defer wg.Done()
+	// Do http GET request.
+	resp, err := http.Get(uri)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// Close response body.
+	defer resp.Body.Close()
+	// Read response body.
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	count := bytes.Count(body, []byte("Go"))
+	mux.Lock()
+	// Only one goroutine at a time can access the total variable.
+	*total += count
+	mux.Unlock()
+
+	log.Printf("Count for %s %d", uri, count)
+}
+
+func ValidUrl(uri string) {
+	_, err := url.ParseRequestURI(uri)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
 func main() {
 
 	var (
-		count int
 		total int
 		uri   string
+		wg    sync.WaitGroup
 		mux   sync.Mutex
 	)
 
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for scanner.Scan() {
-
-		// Get url from stdin.
-		uri = scanner.Text()
-
-		// Check if user finished typing.
-		if uri == "" {
-			break
-		}
-		// uri is valid
-		_, err := url.ParseRequestURI(uri)
-		if err != nil {
-			fmt.Println("Invalid URL, please try again.")
-			// log.Fatalln(err)
-			continue
-		}
-
-		go func() {
-			// Do http GET request.
-			resp, err := http.Get(uri)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			// Close response body.
-			defer resp.Body.Close()
-			// Read response body.
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			mux.Lock()
-			// Only one goroutine at a time can access the count and total vars.
-			count = bytes.Count(body, []byte("Go"))
-			total += count
-			mux.Unlock()
-			log.Printf("Count for %s %d", uri, count)
-		}()
-
-	}
-
-	if err := scanner.Err(); err != nil {
+	// Read stdin.
+	_bytes, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
 		log.Fatalln(err)
 	}
 
+	// Convert bytes to string.
+	toStr := string(_bytes)
+
+	// Split string by \n
+	splited := strings.Split(toStr, "\n")
+	// Delete empty strings from a slice.
+	for i := 0; i < len(splited); i++ {
+		if splited[i] == "" {
+			splited = append(splited[:i], splited[i+1:]...)
+		}
+	}
+
+	for i := 0; i < len(splited); i++ {
+		uri = splited[i]
+		// uri is valid
+		ValidUrl(uri)
+		// Increment the WaitGroup counter.
+		wg.Add(1)
+		go HandleUri(uri, &total, &wg, &mux)
+	}
+
+	// Wait for complete goroutines.
+	wg.Wait()
 	log.Printf("Total: %v", total)
 }
