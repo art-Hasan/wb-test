25d5b10a7965e444ff644ac60d8cc1c35ae1542a
art-Hasan <guseyin.gasimov@gmail.com>
2017-09-27 15:39:28 +0300

    Updated main.go

--- src/cmd/wb-test/main.go
+++ src/cmd/wb-test/main.go
@@ -2,36 +2,75 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"sync"
 )
 
 func init() {
 	log.SetFlags(0)
 }
 
-// Other funcs, if any.
-
 func main() {
 
-	// Initialization, if any.
+	var (
+		count int
+		total int
+		uri   string
+		mux   sync.Mutex
+	)
 
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
 
-		// Do something with strings here.
+		// Get url from stdin.
+		uri = scanner.Text()
+
+		// Check if user finished typing.
+		if uri == "" {
+			break
+		}
+		// uri is valid
+		_, err := url.ParseRequestURI(uri)
+		if err != nil {
+			fmt.Println("Invalid URL, please try again.")
+			// log.Fatalln(err)
+			continue
+		}
+
+		go func() {
+			// Do http GET request.
+			resp, err := http.Get(uri)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			// Close response body.
+			defer resp.Body.Close()
+			// Read response body.
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			mux.Lock()
+			// Only one goroutine at a time can access the count and total vars.
+			count = bytes.Count(body, []byte("Go"))
+			total += count
+			mux.Unlock()
+			log.Printf("Count for %s %d", uri, count)
+		}()
 
 	}
+
 	if err := scanner.Err(); err != nil {
 		log.Fatalln(err)
 	}
-
-	// Other code, if any.
-
-	total := 0
-
-	// Other code, if any.
 
 	log.Printf("Total: %v", total)
 }
